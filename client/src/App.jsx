46b99ab3eaf294ab3008b07c807f4ca1c8c22876
Rajeev Khati <rajeevkhati5312@gmail.com>
2025-04-03 06:14:08 +0530

    add login, register pages | install packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Initial Setup</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
