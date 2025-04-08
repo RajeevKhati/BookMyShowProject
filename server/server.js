ca1065c67701f4dcfee4f1b7297eae550de19479
Rajeev Khati <rajeevkhati5312@gmail.com>
2025-04-08 22:17:10 +0530

    init server code, connect to db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.listen(8082, () => {
+require("dotenv").config(); // Load environment variables
+
+const connectDB = require("./config/db"); // Import database configuration
+const userRouter = require("./routes/userRoutes"); // Import user routes
+
+console.log("server", process.env.DB_URL);
+connectDB(); // Connect to database
+
+/** Routes */
+app.use(express.json());
+app.use("/api/users", userRouter);
+
+app.listen(3001, () => {
   console.log("Server is Running");
 });
